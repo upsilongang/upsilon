0f3836589510fb3170f15e0a5c86ddce6efb517e
super <awesomegamer156@gmail.com>
2021-08-20 20:10:07 -0400

    lore text changes
    * can handle newlines now
    * added comments for readability

--- src/main/java/org/upsilongang/upsilon/util/UpsilonUtil.java
+++ src/main/java/org/upsilongang/upsilon/util/UpsilonUtil.java
@@ -12,31 +12,86 @@ public class UpsilonUtil
     public static List<String> loreText(String s)
     {
         List<String> lines = new ArrayList<>();
+
+        // keep track of the color and line of the lore
         ChatColor currentColor = ChatColor.GRAY;
         StringBuilder currentString = new StringBuilder();
+
+        // split off string into parts so it's easier to manage
         for (String part : s.split(" "))
         {
-            if (currentString.length() + part.length() > 32)
+            if (currentString.length() + part.length() > 32) // is the content of this part too large to fit in the current string?
             {
+                // if so, break off the current string
                 lines.add(currentString.toString());
                 currentString = new StringBuilder();
             }
+
             boolean first = currentString.length() == 0;
-            if (first)
-                currentString.append(currentColor);
+            if (first) // are we at the beginning of the current string?
+                currentString.append(currentColor); // if so, add the current color
             else
-                currentString.append(" ");
-            currentString.append(part);
-            int colorCodeIndex = part.indexOf("§");
-            if (colorCodeIndex != -1 && colorCodeIndex + 1 < part.length())
+                currentString.append(" "); // otherwise, add a space
+
+            // split off part by newline
+            String[] nls = part.split("\n");
+
+            if (nls.length <= 1) // does this part have no newline or one at the end?
             {
-                ChatColor newColor = ChatColor.getByChar(part.charAt(colorCodeIndex + 1));
-                if (newColor != null)
-                    currentColor = newColor;
+                currentString.append(part);
+
+                // change color if necessary
+                int colorCodeIndex = part.lastIndexOf("§");
+                if (colorCodeIndex != -1 && colorCodeIndex + 1 < part.length())
+                {
+                    ChatColor newColor = ChatColor.getByChar(part.charAt(colorCodeIndex + 1));
+                    if (newColor != null)
+                        currentColor = newColor;
+                }
+            }
+            else
+            {
+                // add the first line and break it
+                currentString.append(nls[0]);
+                lines.add(currentString.toString());
+                currentString = new StringBuilder();
+
+                // loop through the rest of the lines with a newline
+                for (int i = 1; i < nls.length; i++)
+                {
+                    String nl = nls[i];
+                    currentString.append(nl);
+
+                    // check for the color changes and apply em if need be
+                    int colorCodeIndex = nl.lastIndexOf("§");
+                    if (colorCodeIndex != -1 && colorCodeIndex + 1 < nl.length())
+                    {
+                        ChatColor newColor = ChatColor.getByChar(nl.charAt(colorCodeIndex + 1));
+                        if (newColor != null)
+                            currentColor = newColor;
+                    }
+
+                    if (i != nls.length - 1) // are there more lines after this one?
+                    {
+                        // if so, break it off
+                        lines.add(currentString.toString());
+                        currentString = new StringBuilder().append(currentColor);
+                    }
+                }
+            }
+
+            if (part.endsWith("\n")) // do we have a newline at the end?
+            {
+                // if so, start new line
+                lines.add(currentString.toString());
+                currentString = new StringBuilder();
             }
         }
+
+        // add current string if there's still another one to be added
         if (currentString.length() != 0)
             lines.add(currentString.toString());
+
         return lines;
     }
 
